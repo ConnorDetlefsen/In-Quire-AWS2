d63a9b698470b4dc20feb5ab0b5bf3096b7fec85
Connor Detlefsen <Detlefsen@chapman.edu>
2020-08-07 02:49:58 -0700

    in-quire@1.0

--- src/Pages/overviewComponent.jsx
+++ src/Pages/overviewComponent.jsx
@@ -100,7 +100,7 @@ class overviewComponent extends Component {
                           <iframe
                             width="560"
                             height="315"
-                            src="https://www.youtube.com/embed/viXSi0JKWsA"
+                            src="https://www.youtube.com/embed/0MqulzRXbUA"
                             frameborder="0"
                             allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                             allowfullscreen
